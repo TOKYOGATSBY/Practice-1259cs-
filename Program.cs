25ef0b60fcb4a986790843b420bd0fde6dd1deb0
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-04-11 08:16:16 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static long EulerPhi(long n)
+    {
+        long result = n;
+        for (long p = 2; p * p <= n; p++)
+        {
+            if (n % p == 0)
+            {
+                while (n % p == 0)
+                {
+                    n /= p;
+                }
+                result -= result / p;
+            }
+        }
+
+        if (n > 1)
+        {
+            result -= result / n;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        long N = long.Parse(Console.ReadLine());
+        long phiN = EulerPhi(N);
+        Console.WriteLine(phiN / 2);
+    }
+}
